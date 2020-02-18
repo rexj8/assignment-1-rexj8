7c6449d59d24eac6c9a93d737e52a05cedb307ad
TheEternalShenron <rachit.gla_cs18@gla.ac.in>
2020-02-18 08:22:49 +0530

    creating main menu in Main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,16 @@
 package main;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Contact List App");
+        System.out.println("Press 1 to add a new contact");
+        System.out.println("Press 2 to view all contact");
+        System.out.println("Press 3 to search for a contact");
+        System.out.println("Press 4 to delete a contact");
+        System.out.println("Press 5 to exit a program");
+
+
+    }
 }
