ed80acb4c27f2918d1cc3b71449c539b4e7a74de
TheEternalShenron <rachit.gla_cs18@gla.ac.in>
2020-02-19 07:21:03 +0530

    created add method in Person class

--- src/definition/LinkedList.java
+++ src/definition/LinkedList.java
@@ -17,9 +17,45 @@ public class LinkedList<E> implements LinkedListADT<E> {
     }
 
 
+    private void addFirst(E item) {
+        head = new Node<>(item, head);
+        size++;
+    }
+
+    private void addAfter(Node<E> node, E item) {
+
+        node.next = new Node<>(item, node.next);
+        size++;
+    }
+
+    public void add(int index, E item) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException(Integer.toString(index));
+        } else if (index == 0) {
+            addFirst(item);
+        } else {
+            Node<E> temp = getNode(index - 1);
+            addAfter(temp, item);
+        }
+    }
+
     @Override
     public void add(E item) {
+        add(size, item);
+    }
 
+    private E removeFirst() {
+        E response = null;
+        Node<E> temp = head;
+        if (head != null) {
+            head = head.getNext();
+        }
+        if (temp != null) {
+            size--;
+            response = temp.getData();
+        }
+
+        return response;
     }
 
     @Override
