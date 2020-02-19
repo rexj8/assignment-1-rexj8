49d58ce51fa70fd346459c6ff5078e336f14b936
TheEternalShenron <rachit.gla_cs18@gla.ac.in>
2020-02-19 07:16:42 +0530

    updating LinkedListADT

--- src/adt/LinkedListADT.java
+++ src/adt/LinkedListADT.java
@@ -1,4 +1,17 @@
 package adt;
 
 public interface LinkedListADT<E> {
+
+    void add(E item);
+
+    E remove();
+
+    int search(E item);
+
+    void sort();
+
+    void print();
+
+    E get(int index);
+
 }
--- src/definition/Person.java
+++ src/definition/Person.java
@@ -4,14 +4,19 @@ import adt.PersonADT;
 
 public class Person implements PersonADT {
     LinkedList<String> ls = new LinkedList<>();
+    LinkedList<String> linkedListFName = new LinkedList<>();
+    LinkedList<String> linkedListLName = new LinkedList<>();
     String fname;
     String lname;
     String email;
 
     @Override
     public void addName(String fname, String lname) {
+
         this.fname = fname;
         this.lname = lname;
+
+//        linkedListFName.add(fname);
     }
 
     @Override
