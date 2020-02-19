efc786d7f4d8cd23f538bf51da9cd00c48c0875d
TheEternalShenron <rachit.gla_cs18@gla.ac.in>
2020-02-19 07:18:11 +0530

    overriding adt methods in LinkedList  class

--- src/definition/LinkedList.java
+++ src/definition/LinkedList.java
@@ -4,4 +4,33 @@ import adt.LinkedListADT;
 
 public class LinkedList<E> implements LinkedListADT<E> {
 
+    @Override
+    public void add(E item) {
+
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public int search(E item) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
 }
